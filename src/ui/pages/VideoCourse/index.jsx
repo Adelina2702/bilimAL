728f358eaa2262b10f4e1387bc2dcb776a2b88e5
Adelina <adelina@MacBook-Pro-User-2.local>
2023-09-18 00:16:21 +0600

    change layout

--- src/ui/pages/VideoCourse/index.jsx
+++ src/ui/pages/VideoCourse/index.jsx
@@ -6,70 +6,73 @@ import Footer from '../../components/Footer';
 
 const VideoCourse = () => {
   return (
-<div className='w-[100%]'>
-    <div className='flex flex-col mx-[100px] mt-[50px]'>
-      <div className='flex mb-[40px]'>
-        <iframe
-          width='762'
-          height='468'
-          borderRadius='20'
-          src='https://www.youtube.com/embed/awHEjo7xAzs?si=CxUSQA20lgq2pq45'
-          title='YouTube video player'
-          frameborder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowfullscreen></iframe>
-        <div className='ml-[58px]'>
-          <h4 className='text-[24px] font-semibold mb-[20px]'>Тема урока</h4>
-          <div className='flex w-[418px] h-[68px] rounded-lg bg-[#54A3FF] p-[10px] mb-[40px]'>
-            <Reproduce />
-            <h4 className='text-[20px] text-[#ffff] font-semibold ml-[20px] leading-7'>
-              Знакомство с приложением Tik Tok
-            </h4>
-          </div>
-          <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
-            <Pause />
-            <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
-              Знакомство с интсрументом видео 2
-            </h4>
-          </div>
-          <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
-            <Pause />
-            <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
-              Знакомство с интсрументом видео 3
-            </h4>
-          </div>
-          <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
-            <Pause />
-            <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
-              Знакомство с интсрументом видео 4
-            </h4>
+    <div className='w-[100%]'>
+      <div className='flex flex-col mx-[100px] mt-[50px]'>
+        <div className='flex mb-[40px]'>
+          <iframe
+            width='762'
+            height='468'
+            borderRadius='20'
+            src='https://www.youtube.com/embed/J12TUwE-Kek?si=zlwovqp2ibbblr-A'
+            title='YouTube video player'
+            frameborder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowfullscreen></iframe>
+          <div className='ml-[58px]'>
+            <h4 className='text-[24px] font-semibold mb-[20px]'>Тема урока</h4>
+            <div className='flex w-[418px] h-[68px] rounded-lg bg-[#54A3FF] p-[10px] mb-[40px]'>
+              <Reproduce />
+              <h4 className='text-[20px] text-[#ffff] font-semibold ml-[20px] leading-7'>
+                Знакомство с персональными данными
+              </h4>
+            </div>
+            <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
+              <Pause />
+              <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
+                Знакомство с интсрументом видео 2
+              </h4>
+            </div>
+            <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
+              <Pause />
+              <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
+                Знакомство с интсрументом видео 3
+              </h4>
+            </div>
+            <div className='flex w-[418px] h-[68px] rounded-lg bg-[#F6F5FF] p-[10px] mb-[40px]'>
+              <Pause />
+              <h4 className='text-[20px] font-semibold ml-[20px] leading-7'>
+                Знакомство с интсрументом видео 4
+              </h4>
+            </div>
           </div>
         </div>
+        <div className=' flex w-[764px] h-[114px] rounded-lg border border-solid border-[#3FA4EA] mb-[49px]'>
+          <div className='mx-[22px] my-[15px]'>
+            <h4 className='font-semibold text-[24px] mb-[12px]'>Уровень</h4>
+            <h5 className='text-[20px]'>Начинающий</h5>
+          </div>
+          <Line />
+          <div className='mx-[22px] my-[15px]'>
+            <h4 className='font-semibold text-[24px] mb-[12px]'>
+              Длительность
+            </h4>
+            <h5 className='text-[20px]'>всего 1 час 30 мин</h5>
+          </div>
+          <Line />
+          <div className='mx-[22px] my-[15px]'>
+            <h4 className='font-semibold text-[24px] mb-[12px]'>Язык</h4>
+            <h5 className='text-[20px]'>Русский / Кыргызкий</h5>
+          </div>
+        </div>
+        <h5 className='text-[30px] font-semibold mb-[20px]'>Описание урока</h5>
+        <p className='text-[20px] font-medium w-[762px] mb-[20px]'>
+          На первом занятии вы научитесь искать уязвимости, отработаете знания
+          на проектах, приближенных к реальным рабочим ситуациям, рассмотрите
+          предотвращегие угроз и обеспечиванию безопасности
+        </p>
       </div>
-      <div className=' flex w-[764px] h-[114px] rounded-lg border border-solid border-[#3FA4EA] mb-[49px]'>
-        <div className='mx-[22px] my-[15px]'>
-          <h4 className='font-semibold text-[24px] mb-[12px]'>Уровень</h4>
-          <h5 className='text-[20px]'>Начинающий</h5>
-        </div>
-        <Line />
-        <div className='mx-[22px] my-[15px]'>
-          <h4 className='font-semibold text-[24px] mb-[12px]'>Длительность</h4>
-          <h5 className='text-[20px]'>всего 1 час 30 мин</h5>
-        </div>
-        <Line />
-        <div className='mx-[22px] my-[15px]'>
-          <h4 className='font-semibold text-[24px] mb-[12px]'>Язык</h4>
-          <h5 className='text-[20px]'>Русский / Кыргызкий</h5>
-        </div>
-      </div>
-      <h5 className='text-[30px] font-semibold mb-[20px]'>Описание урока</h5>
-      <p className='text-[20px] font-medium w-[762px] mb-[20px]'>
-        Познакомьтесь с нашим аналитическим инструментом. Узнайте больше об
-        особенностях TikTok, его интересах и общем отношении к брендам.
-      </p>
+      <Footer />
     </div>
-      <Footer/>
-      </div>
   );
 };
 
--- src/ui/pages/View/index.jsx
+++ src/ui/pages/View/index.jsx
@@ -11,72 +11,71 @@ import Footer from '../../components/Footer';
 const View = () => {
   return (
     <div>
-    <div className='flex flex-col mx-[100px]'>
-      <div className='flex mt-[60px] mb-[110px]'>
-        <img src={TikTok} alt='' />
-        <div className='flex flex-col mx-[43px] my-[15px]'>
-          <div className='text-[30px] font-semibold mb-[15px]'>
-            Подробный курс по Tik Tok
+      <div className='flex flex-col mx-[100px]'>
+        <div className='flex mt-[60px] mb-[110px]'>
+          <img src={TikTok} alt='' />
+          <div className='flex flex-col mx-[43px] my-[15px]'>
+            <div className='text-[30px] font-semibold mb-[15px]'>
+              Подробный курс по персональным данным
+            </div>
+            <div className='text-[21px] mb-[30px]'>
+              Данный курс предлагает комплексное погружение в сферу цифровой
+              безопасности, предоставляя студентам необходимые знания и навыки
+              для эффективного обеспечения безопасности информационных активов.
+              В ходе обучения вы ознакомятся с основными аспектами безопасности
+              активов, инжиниринга.
+            </div>
+            <Button text={'Записаться на курс'} />
           </div>
-          <div className='text-[21px] mb-[30px]'>
-            Курс 'Мастерство TikTok с нуля' поможет вам научиться создавать
-            захватывающий контент и эффективно использовать эту популярную
-            социальную платформу. Весь базовый набор навыков и инструментов для
-            успешного присутствия на TikTok, включая создание увлекательных
-            видеороликов, редактирование, добавление музыки и спецэффектов,
-            оптимизацию профиля и привлечение аудитории - все это вы найдете в
-            курсе 'Мастерство TikTok с нуля'.
+        </div>
+        <div className='flex'>
+          <div className='flex flex-col'>
+            <div className='text-[22px] font-semibold mb-[30px] cursor-pointer'>
+              Чему вы научитесь
+              <span className='text-[#8A8A8A] ml-[40px] cursor-pointer'>
+                Отзывы учащихся
+              </span>
+            </div>
+            <div className='w-[695px] h-[188px] text-[23px]'>
+              Курс обеспечивает практическую ориентацию, позволяя развивать
+              навыки анализа рисков, разработки безопасных систем, управления
+              уязвимостями, а также эффективного реагирования на инциденты
+              безопасности.
+            </div>
           </div>
-          <Button text={'Записаться на курс'} />
+          <div className='flex flex-col ml-[56px]'>
+            <div className='text-[22px] font-semibold mb-[30px]'>
+              Рейтинг курса
+            </div>
+            <div className='flex text-center items-center mb-[12px] text-[20px] flex-wrap'>
+              <h4 className='mr-[10px]'>5 звезд</h4>
+              <Shkala1 />
+              <h4 className='ml-[10px]'>90 %</h4>
+            </div>
+            <div className='flex text-center items-center mb-[12px] text-[20px]'>
+              <h4 className='mr-[10px]'>4 звезд</h4>
+              <Shkala2 />
+              <h4 className='ml-[10px]'>6 %</h4>
+            </div>
+            <div className='flex text-center items-center mb-[12px] text-[20px]'>
+              <h4 className='mr-[10px]'>3 звезд</h4>
+              <Shkala3 />
+              <h4 className='ml-[10px]'>2 %</h4>
+            </div>
+            <div className='flex text-center items-center mb-[12px] text-[20px]'>
+              <h4 className='mr-[10px]'>2 звезд</h4>
+              <Shkala4 />
+              <h4 className='ml-[10px]'>1 %</h4>
+            </div>
+            <div className='flex text-center items-center mb-[12px] text-[20px]'>
+              <h4 className='mr-[10px]'>1 звезд</h4>
+              <Shkala5 />
+              <h4 className='ml-[10px]'>1 %</h4>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='flex'>
-        <div className='flex flex-col'>
-          <div className='text-[22px] font-semibold mb-[30px] cursor-pointer'>
-            Чему вы научитесь
-            <span className='text-[#8A8A8A] ml-[40px] cursor-pointer'>Отзывы учащихся</span>
-          </div>
-          <div className='w-[695px] text-[23px]'>
-            В процессе обучения вы узнаете о типах аккаунта, стратегиях
-            продвижения, алгоритмах, ключевых словах и вирусном контенте.
-            Научитесь снимать и монтировать, проводить прямые эфиры и автоэфиры,
-            монетизировать аккаунт. Курс подойдет не только блоггерам, но и
-            новичкам, начинающим
-          </div>
-        </div>
-        <div className='flex flex-col ml-[56px]'>
-          <div className='text-[22px] font-semibold mb-[30px]'>
-            Рейтинг курса
-          </div>
-          <div className='flex text-center items-center mb-[12px] text-[20px] flex-wrap'>
-            <h4 className='mr-[10px]'>5 звезд</h4>
-            <Shkala1 />
-            <h4 className='ml-[10px]'>90 %</h4>
-          </div>
-          <div className='flex text-center items-center mb-[12px] text-[20px]'>
-            <h4 className='mr-[10px]'>4 звезд</h4>
-            <Shkala2 />
-            <h4 className='ml-[10px]'>6 %</h4>
-          </div>
-          <div className='flex text-center items-center mb-[12px] text-[20px]'>
-            <h4 className='mr-[10px]'>3 звезд</h4>
-            <Shkala3 />
-            <h4 className='ml-[10px]'>2 %</h4>
-          </div>
-          <div className='flex text-center items-center mb-[12px] text-[20px]'>
-            <h4 className='mr-[10px]'>2 звезд</h4>
-            <Shkala4 />
-            <h4 className='ml-[10px]'>1 %</h4>
-          </div>
-          <div className='flex text-center items-center mb-[12px] text-[20px]'>
-            <h4 className='mr-[10px]'>1 звезд</h4>
-            <Shkala5 />
-            <h4 className='ml-[10px]'>1 %</h4>
-          </div>
-        </div>
-      </div>
-    </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 };
